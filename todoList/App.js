529f986aba9cc6e71e2bd11348431b442ef77a83
mulanwanderingearth <happyxin331@icloud.com>
2025-04-03 14:08:15 -0700

    add functions of add task and delete task

--- todoList/App.js
+++ todoList/App.js
@@ -1,20 +1,61 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Task from './components/Task';
+import React, {useState} from "react";
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Platform,
+  Keyboard,
+} from "react-native";
+import Task from "./components/Task";
+
 
 export default function App() {
+  const [task, setTask]=useState();
+  const [taskItems, setTaskItems]=useState([]);
+
+  const handleAddTask=()=>{
+    Keyboard.dismiss();
+    setTaskItems([...taskItems,task])
+    setTask(null);
+  }
+
+  const completeTask=(index)=>{
+    let itemsCopy=[...taskItems];
+    itemsCopy.splice(index,1);
+    setTaskItems(itemsCopy);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.taskWrapper}>
         <Text style={styles.sectionTitle}>Today's tasks</Text>
         <View style={styles.items}>
-
-         <Task text={'Task 1'} />
-         <Task text={'Task 2'} />
-
-
+          {taskItems.map((item, index)=>{
+            return (
+              <TouchableOpacity key={index} onPress={()=>completeTask(index)}>
+              <Task text={item}/>
+              </ TouchableOpacity>
+            )
+           
+          })
+          }
         </View>
       </View>
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS == "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={"Write a task"} value={task} onChangeText={text =>setTask(text)}/>
+        <TouchableOpacity onPress={()=>handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -22,7 +63,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E8EAED',
+    backgroundColor: "#E8EAED",
   },
   taskWrapper: {
     paddingTop: 80,
@@ -30,10 +71,37 @@ const styles = StyleSheet.create({
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   items: {
-    marginTop:30,
-
+    marginTop: 30,
   },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+  },
+  addText: {},
 });
